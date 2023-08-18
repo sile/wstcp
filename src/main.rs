15a0a4d5e3fd3bb46fb1de58d37387c3d165542b
Takeru Ohta <phjgt308@gmail.com>
2023-08-18 22:42:00 +0900

    Update clap to v4

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,48 @@
-#[macro_use]
 extern crate clap;
 #[macro_use]
 extern crate trackable;
 
 use async_std::net::TcpListener;
-use clap::Arg;
+use clap::{Parser, ValueEnum};
 use slog::o;
 use sloggers::terminal::{Destination, TerminalLoggerBuilder};
 use sloggers::types::SourceLocation;
 use sloggers::Build;
-use std::net::{SocketAddr, ToSocketAddrs};
+use std::net::SocketAddr;
 use wstcp::{Error, ProxyServer};
 
-macro_rules! try_parse {
-    ($expr:expr) => {
-        track_any_err!($expr.parse())
-    };
+#[derive(Parser)]
+struct Args {
+    /// The TCP address of the real server.
+    real_server_addr: SocketAddr,
+
+    /// Log level.
+    #[clap(long, default_value = "info")]
+    log_level: LogLevelArg,
+
+    /// TCP address to which the WebSocket proxy bind.
+    #[clap(long, default_value = "0.0.0.0:13892")]
+    bind_addr: SocketAddr,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, ValueEnum)]
+enum LogLevelArg {
+    Debug,
+    Info,
+    Warning,
+    Error,
 }
 
 fn main() -> trackable::result::TopLevelResult {
-    let matches = app_from_crate!()
-        .arg(
-            Arg::with_name("REAL_SERVER_ADDR")
-                .help("The TCP address of the real server")
-                .index(1)
-                .takes_value(true)
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("LOG_LEVEL")
-                .long("log-level")
-                .takes_value(true)
-                .default_value("info")
-                .possible_values(&["debug", "info", "warning", "error"]),
-        )
-        .arg(
-            Arg::with_name("BIND_ADDR")
-                .help("TCP address to which the WebSocket proxy bind")
-                .long("bind-addr")
-                .takes_value(true)
-                .default_value("0.0.0.0:13892"),
-        )
-        .get_matches();
-
-    let bind_addr: SocketAddr = try_parse!(matches.value_of("BIND_ADDR").unwrap())?;
-    let tcp_server_addr: SocketAddr = track_assert_some!(
-        track_any_err!(matches
-            .value_of("REAL_SERVER_ADDR")
-            .unwrap()
-            .to_socket_addrs())?
-        .next(),
-        trackable::error::Failed
-    );
-    let log_level = try_parse!(matches.value_of("LOG_LEVEL").unwrap())?;
+    let args = Args::parse();
+    let bind_addr = args.bind_addr;
+    let tcp_server_addr = args.real_server_addr;
+    let log_level = match args.log_level {
+        LogLevelArg::Debug => sloggers::types::Severity::Debug,
+        LogLevelArg::Info => sloggers::types::Severity::Info,
+        LogLevelArg::Warning => sloggers::types::Severity::Warning,
+        LogLevelArg::Error => sloggers::types::Severity::Error,
+    };
     let logger = track!(TerminalLoggerBuilder::new()
         .source_location(SourceLocation::None)
         .destination(Destination::Stderr)
@@ -71,6 +61,5 @@ fn main() -> trackable::result::TopLevelResult {
             .unwrap_or_else(|e| panic!("{}", e));
         proxy.await.unwrap_or_else(|e| panic!("{}", e));
     });
-
     Ok(())
 }
